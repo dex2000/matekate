a221ee5556bb75af768dfaf7f89cfd2d50dd1c2c
Sven Fischer <git-dev@linux4tw.de>
2015-04-16 22:16:31 +0200

    icon definition

---
+++ js/icons.js
@@ -0,0 +1,4 @@
+var icon_folder = 'icons';
+var icon_normal = L.icon({iconUrl: icon_folder + '/club-mate_24x24_-12x-12.png', iconSize: [24, 24] });
+var icon_retail = L.icon({iconUrl: icon_folder + '/club-mate-retail_30x40_-12x-28.png', iconSize: [30, 40] });
+var icon_served = L.icon({iconUrl: icon_folder + '/club-mate-served_32x40_-12x-28.png', iconSize: [32, 40] });
