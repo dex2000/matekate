73d15a7ec2ae3b1ba69469d1ccde9ef44cd9b9e5
Sven Fischer <git-dev@linux4tw.de>
2015-04-20 22:47:02 +0200

    add infobox plugin

--- js/leaflet/leaflet.infobox.js
+++ js/leaflet/leaflet.infobox.js
@@ -13,10 +13,41 @@
       window.L.YourPlugin = factory(L);
   }
 }(function (L) {
-  var MyLeafletPlugin = {};
-  // implement your plugin
+  L.Control.Infobox = L.Control.extend({
+    options: {
+      position: 'topleft',
+      icon: 'fa fa-info'
+    },
 
-  // return your plugin when you are done
-  return MyLeafletPlugin;
+    initialize: function (options) {
+      L.Util.setOptions(this, options;
+    },
+
+    onAdd: function (map) {
+      var container = L.DomUtil.create('div','leaflet-control-infobox leaflet-bar leaflet-control');
+      this._link = L.DomUtil.create('a', 'leaflet-bar-part leaflet-bar-part-single', container);
+      this._link.href = '#';
+      this._icon = L.DomUtil.create('span', this.options.icon, this._link);
+      this._map = map;
+      this._container = container;
+
+      L.DomEvent.on(this._link, 'click',
+        function() {
+          this.div = document.getElementById("infobox");
+          if (this.div.style.display == "" || this.div.style.display == "none") {
+            this.div.style.display = "block";
+          }
+          else {
+            this.div.style.display = "none";
+          }
+        }
+        , this);
+      return container;
+    }
+  });
+
+  L.control.infobox = function (options) {
+      return new L.Control.Infobox(options);
+  };
 }, window));
 
